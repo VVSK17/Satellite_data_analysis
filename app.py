23c1c009c4625487d59777defba811edae5b7f06
VVSK17 <vuppalapatisannithkumarnaidu@gmail.com>
2025-05-19 20:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import cv2
 import skimage.registration
-register_translation = skimage.registration.register_translation
 from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
